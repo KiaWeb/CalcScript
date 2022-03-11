edb2791eac2898c21bb42cc62ac6f49320077313
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-03-10 19:21:52 -0600

    Update index.py

--- index.py
+++ index.py
@@ -7,9 +7,24 @@ Much will be introduced in later versions of CalcScript.
 print(intromsg)
 cmdlist = ["help","add","mult","sub","div"] # How CalcScript's command list works
 interpreter = "CALCSCRIPT>"
+def runCmd(command):
+  if command in cmdlist:
+    if command == "help":
+      import mhelp
+    elif command == "add":
+      import add
+    elif command == "mult":
+      import mult
+    elif command == "sub":
+      import sub
+    elif command == "div":
+      import div
+
 def getc():
   cmd = input(interpreter)
   if cmd in cmdlist:
     runCmd(cmd)
+    getc()
    else:
     raise CommandError("Not a command.")
+    getc()
