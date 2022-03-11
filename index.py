86cbafff34984c9a9e3dd5c55dfee599a5d444c6
Kia <82962111+KiaWeb@users.noreply.github.com>
2022-03-10 19:11:27 -0600

    Start of the v0.0.1

--- index.py
+++ index.py
@@ -5,9 +5,11 @@ CalcScript is a programming language based on Python and calculation.
 Much will be introduced in later versions of CalcScript.
 '''
 print(intromsg)
-cmdlist = {"help":"help.py"} # How CalcScript's command list works (Whatever command name, returns module import name.
-interpreter = "CalcScript>"
+cmdlist = ["help","add","mult","sub","div"] # How CalcScript's command list works
+interpreter = "CALCSCRIPT>"
 def getc():
   cmd = input(interpreter)
   if cmd in cmdlist:
-    
+    runCmd(cmd)
+   else:
+    raise CommandError("Not a command.")
