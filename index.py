0e62e8438722c8828916d74721954b85c2966c48
BuildTools <unconfigured@null.spigotmc.org>
2022-03-11 08:22:23 -0600

    interpreter clear added

--- index.py
+++ index.py
@@ -1,24 +1,37 @@
 #!python3
+import os
+import platform
 intromsg = '''
 Copyright (C) 2022-(present) CalcScript Authors.
 CalcScript is a programming language based on Python and calculation.
 Much will be introduced in later versions of CalcScript.
 '''
 print(intromsg)
-cmdlist = ["help","add","mult","sub","div"] # How CalcScript's command list works
+clstypes = ["cls", "clear", "clr"]
+cmdlist = ["help","eval", "cls", "clear", "clr"] # How CalcScript's command list works
 interpreter = "CALCSCRIPT>"
+def lClear():
+  os.system("clear")
+  getc()
+
+def wClear():
+  os.system("cls")
+  getc()
+
 def runCmd(command):
+  command = command.lower()
   if command in cmdlist:
-    if command == "help":
+    if command == "eval":
+      import eval
+    elif command == "help":
       import mhelp
-    elif command == "add":
-      import add
-    elif command == "mult":
-      import mult
-    elif command == "sub":
-      import sub
-    elif command == "div":
-      import div
+    elif command in clstypes:
+      if platform.system() == "Linux" or platform.system() == "Darwin":
+        lClear()
+      else:
+        wClear()
+
+
 
 def getc():
   cmd = input(interpreter)
@@ -26,7 +39,7 @@ def getc():
     runCmd(cmd)
     getc()
   else:
-    raise CommandError("Not a command.")
+    raise Exception("Not a command.")
     getc()
 
 getc()
